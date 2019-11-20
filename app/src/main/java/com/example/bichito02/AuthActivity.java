ee9c5722e7d566b599a0c9d8da31cd6853367a79
LAB_F_REAL <labf@continental.edu.pe>
2019-11-20 18:33:00 -0500

    clase 06

---
+++ app/src/main/java/com/example/bichito02/AuthActivity.java
@@ -0,0 +1,33 @@
+package com.example.bichito02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AuthActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText txtUser;
+    EditText txtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtUser = findViewById(R.id.txtUser);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtUser.getText().toString().equals("Nory")) {
+                    Intent o = new Intent(AuthActivity.this, MainActivity.class);
+                    startActivity(o);
+                }
+            }
+        });
+    }
+}
